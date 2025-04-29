f29da566678b6fa5ed6328d682dead31810958a5
Rangga857 <azharrangga28@gmail.com>
2025-04-29 18:21:22 +0700

    Inisialisasi format tanggal lokal

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:ucp1/login_page.dart';
-import 'package:ucp1/register_page.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('id_ID', null);
   runApp(const MyApp());
 }
 
