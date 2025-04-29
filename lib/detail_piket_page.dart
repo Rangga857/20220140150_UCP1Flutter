1da7727e2d6e424b3fb2992cf5aadda38f8bff34
Rangga857 <azharrangga28@gmail.com>
2025-04-29 16:40:31 +0700

    create variabe dan parameter pafa kontruktor detailpiket

---
+++ lib/detail_piket_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DetailPiketPage extends StatelessWidget {
+  final String tugas;
+  final String tanggal;
+  final String nama;
+  const DetailPiketPage({
+    super.key,
+    required this.tugas,
+    required this.tanggal,
+    required this.nama,});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
