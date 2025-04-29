9a6a583dc74e5e6e078de23f4e1b190a79ab4292
Rangga857 <azharrangga28@gmail.com>
2025-04-29 18:01:42 +0700

    create body untuk pelanggan page

--- lib/data_pelanggan_page.dart
+++ lib/data_pelanggan_page.dart
@@ -8,26 +8,50 @@ class PelangganPage extends StatefulWidget {
 }
 
 class _PelangganPageState extends State<PelangganPage> {
+  final TextEditingController namaController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController nohpController = TextEditingController();
+  final TextEditingController alamatController = TextEditingController();
+  final TextEditingController provinsiController = TextEditingController();
+  final TextEditingController kodeposController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+
+  void _resetForm() {
+    namaController.clear();
+    emailController.clear();
+    nohpController.clear();
+    alamatController.clear();
+    provinsiController.clear();
+    kodeposController.clear();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final TextEditingController namaController = TextEditingController();
-    final TextEditingController emailController = TextEditingController();
-    final TextEditingController nohpController = TextEditingController();
-    final TextEditingController alamatController = TextEditingController();
-    final TextEditingController provinsiController = TextEditingController();
-    final TextEditingController kodeposController = TextEditingController();
-
-    final _formKey = GlobalKey<FormState>();
-
-    void _resetForm() {
-      namaController.clear();
-      emailController.clear();
-      nohpController.clear();
-      alamatController.clear();
-      provinsiController.clear();
-      kodeposController.clear();
-    }
-    
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 255, 85, 0),
+        title: const Text('Data Pelanggan', 
+        style: TextStyle(color: Colors.white)),
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [           
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
