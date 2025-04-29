b060125e4b161a05283ada674ad3dd89e7241687
Rangga857 <azharrangga28@gmail.com>
2025-04-29 12:14:51 +0700

    deklarasi variable pada detail barang

---
+++ lib/detail_barang_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  final String tanggal;
+  final String jenisTransaksi;
+  final String jenisBarang;
+  final int jumlahBarang;
+  final int hargaSatuan;
+  final int totalHarga;
+
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
