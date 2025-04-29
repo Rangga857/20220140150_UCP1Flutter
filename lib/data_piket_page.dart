5d56948aaa675d1333f1860278446cedeedaac46
Rangga857 <azharrangga28@gmail.com>
2025-04-29 12:43:56 +0700

    create fungsi untuk add tugas

--- lib/data_piket_page.dart
+++ lib/data_piket_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class DataPiketPage extends StatefulWidget {
   final String nama;
@@ -17,7 +18,33 @@ class _DataPiketPageState extends State<DataPiketPage> {
   List<Map<String, String>> taskList = [];
 
   final _formKey = GlobalKey<FormState>();
-  
+
+   @override
+  void initState() {
+    super.initState();
+    namaController.text = widget.nama;
+  }
+
+  void tambahtugas() {
+    final isValid = _formKey.currentState!.validate();
+    final isDateSelected = selectedDate != null;
+
+    setState(() {
+      if (isValid && isDateSelected) {
+        taskList.add({
+          'task': tugasController.text,
+          'date': DateFormat('EEEE, d MMMM yyyy', 'id_ID').format(selectedDate!),
+          'user': namaController.text,
+        });
+        tugasController.clear();
+        selectedDate = null;
+        dateError = null;
+      } else {
+        dateError = isDateSelected ? null : 'Tanggal harus dipilih';
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
