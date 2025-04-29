e40746953c5f24fd371d05278244b2da14f4ce6d
Rangga857 <azharrangga28@gmail.com>
2025-04-29 11:40:40 +0700

    Create fungsi selected date dengan datepicker

--- lib/data_barang_page.dart
+++ lib/data_barang_page.dart
@@ -1,4 +1,7 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class BarangPage extends StatefulWidget {
   const BarangPage({super.key});
@@ -11,14 +14,14 @@ class _BarangPageState extends State<BarangPage> {
   @override
   Widget build(BuildContext context) {
     TextEditingController jumlahController = TextEditingController();
-    TextEditingController ttanggalController = TextEditingController();
+    TextEditingController tanggalController = TextEditingController();
     DateTime? selectedDate;
     String? selectedTransaksi;
     String? selectedBarang;
     int hargaSatuan = 0;
     int totalHarga = 0;
 
-    final List<String> jenisTransaksi = ['Masuk', 'Keluar'];
+    final List<String> jenisTransaksi = ['Barang Masuk', 'Barang Keluar'];
     final Map<String, int> jenisBarang = {
       'Matras': 100000,
       'Sepatu': 5000000,
@@ -26,6 +29,23 @@ class _BarangPageState extends State<BarangPage> {
       'Sleeping Bag': 200000,
     };
     
+    final _formKey = GlobalKey<FormState>();
+
+    Future<Void> _selectedDate(BuildContext context) async{
+      final DateTime? picked = await showDatePicker(
+        context: context, 
+        initialDate: selectedDate ?? DateTime.now(),
+        firstDate: DateTime(2020), 
+        lastDate: DateTime(2050)
+      );
+      if (picked != null){
+        setState(() {
+          selectedDate = picked;
+          tanggalController.text = DateFormat('EEE, d MMMM yyyy', 'id_ID')
+          .format(picked);
+        });
+      }
+    }
     return const Placeholder();
   }
 }
